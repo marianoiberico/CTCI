8319d0a1fadec03bbba76686e6a1bb15c1f183be
Mariano Iberico Torres <marianoibericot@gmail.com>
2017-12-28 23:32:13 -0800

    Create oneEditAway.py

---
+++ Chapter1-ArraysAndStrings/oneEditAway.py
@@ -0,0 +1,18 @@
+#Code to check if a pair of strings are one edit away (add,delete or change character) from each other
+
+def oneEditAway(s1,s2):
+	if len(s1)<len(s2):
+		#Make sure s1 always hold the larger string
+    s1,s2 = s2,s1
+	if len(s1)-len(s2)>1:
+    #If one of them is more than two characters bigger in length then we can immediatly return false
+		Return false
+  elif len(s1)==len(s2):
+    #if same length then we have to make sure that strings do not differ by more than one character
+		return sum(1 for i in len(s1) if s1(i)!=s2(i))<2
+	else:
+    #if strings differ in length by exactly one, try discarding one character fromt the longer string and see if they match (all possible permutations)
+    for i in len(s1):
+			if s1(:i) + s1(i+1:) == s2:
+				rturn true
+return false
