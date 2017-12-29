d790ee5dfa931567ff8c7d7205c926c9637dec15
Mariano Iberico Torres <marianoibericot@gmail.com>
2017-12-28 23:41:57 -0800

    Update compressString.py

--- Chapter1-ArraysAndStrings/compressString.py
+++ Chapter1-ArraysAndStrings/compressString.py
@@ -1,19 +1,19 @@
 #Function used to compress string given that there are repetitions in letters
 
-def dtringCompression(s):
-
-	output=''
+def stringCompression(s):
+  output=''
   repeat='1'
-	//Add first element 
+  #Add first element 
   output=s[0]
-	
 	for i in range(len(s1)-1):
-	  if s[i]==s[i+1] repeat++
-		else:
-			If(repeat>1) output +=str(repeat)
-			Output+=s[i+1]
-			Repeat=1
-	if(repeat>1):
-		output+=str(repeat)
+    if s[i]==s[i+1] repeat++
+    else:
+      if(repeat>1):
+        output+=str(repeat)
+      output+=s[i+1]
+      repeat=1
+  
+  if(repeat>1):
+    output+=str(repeat)
 		
 print(output)
