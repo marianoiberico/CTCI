0ec02a2611798fb84c61328d5d86195e5d1358c7
Mariano Iberico Torres <marianoibericot@gmail.com>
2017-12-28 23:36:56 -0800

    Update oneEditAway.py

--- Chapter1-ArraysAndStrings/oneEditAway.py
+++ Chapter1-ArraysAndStrings/oneEditAway.py
@@ -2,17 +2,17 @@
 
 def oneEditAway(s1,s2):
   if len(s1)<len(s2):
-	  #Make sure s1 always hold the larger string
+    #Make sure s1 always hold the larger string
     s1,s2 = s2,s1
-	if len(s1)-len(s2)>1:
+  if len(s1)-len(s2)>1:
     #If one of them is more than two characters bigger in length then we can immediatly return false
-		return false
+    return false
   elif len(s1)==len(s2):
-    #if same length then we have to make sure that strings do not differ by more than one character
-		return sum(1 for i in len(s1) if s1(i)!=s2(i))<2
-	else:
-    #if strings differ in length by exactly one, try discarding one character fromt the longer string and see if they match (all possible permutations)
+  #if same length then we have to make sure that strings do not differ by more than one character
+    return sum(1 for i in len(s1) if s1(i)!=s2(i))<2
+  else:
+  #if strings differ in length by exactly one, try discarding one character fromt the longer string and see if they match (all possible permutations)
     for i in len(s1):
-			if s1(:i) + s1(i+1:) == s2:
-				return true
-	return false
+      if s1(:i) + s1(i+1:) == s2:
+        return true
+ return false
